fe46210239ce9a12e00d5d2dfcbe22c65a0b528a
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-01-04 11:44:30 -0300

    Primeiras funções

---
+++ 2_funcoes/1_primeiras_funcoes/scrips.js
@@ -0,0 +1,17 @@
+function imprimeNoConsole (){
+    console.log("Imprimindo no console")
+}
+
+imprimeNoConsole();
+
+const imprime = function(){
+    console.log( "Usando funções anônimas dentro de variáveis.")
+}
+
+imprime();
+
+function funcaoComParametro (texto){
+    console.log('Exemplo: '+texto);
+}
+
+funcaoComParametro('Este é o texto da função')
