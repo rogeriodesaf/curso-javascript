d8cb4d26d06152c794a2604615706f679a4bc7f7
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-01-04 11:01:28 -0300

    Exercicio com estrutura de repetição

---
+++ 1_introducao/exercicio6/scripts.js
@@ -0,0 +1,10 @@
+//Escreva um loop que exiba qual número é par e qual número é impar.
+
+
+for(let i = 0 ; i <=50;i++){
+    if(i % 2 == 0){
+        console.log(i+' é par');
+    }else{
+        console.log(i+' é impar')
+    }
+}
