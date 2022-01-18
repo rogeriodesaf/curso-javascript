2b65cf2cc96583a7bcaddd27afd001897e17bd05
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-01-18 09:31:12 -0300

    reverse

---
+++ arrays/reverse.js
@@ -0,0 +1,9 @@
+let numeros = [1,2,3,4,5,6];
+
+console.log(numeros.reverse());
+
+let nomes = ['Rogério', 'Lohana', 'Rodrigues'];
+
+console.log(nomes.reverse());
+
+/*inverte a ordem dos elementos do array */
