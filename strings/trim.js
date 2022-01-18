458737ba1fefd89e6f7a65282455a98420559f8f
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-01-18 09:41:42 -0300

    método Trim para strings

---
+++ strings/trim.js
@@ -0,0 +1,8 @@
+let nome = "     Rogério."
+
+console.log(`Olá ${nome}`);
+
+nomeCorrigido = nome.trim();
+console.log(`Olá ${nomeCorrigido}`);
+
+/*Método trim retira caracteres especiais ou espaços da string */
