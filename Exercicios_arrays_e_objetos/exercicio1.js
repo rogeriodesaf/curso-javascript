42c84fddbb8dadf0807b9093ab2de39e84b8c1b3
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-01-18 10:50:24 -0300

    Exercicio 1 com arrays

---
+++ Exercicios_arrays_e_objetos/exercicio1.js
@@ -0,0 +1,10 @@
+/* criar um array com 5 itens;
+acessar o item 1,3,4;
+*/
+
+const arr = [0,23,2,3,46];
+for(let i =0 ; i < arr.length ; i++){
+    if( i != 0 && i != 2){
+    console.log(arr[i]);
+}
+};
