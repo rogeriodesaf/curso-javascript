5bfaafd0daaa4b37d1e51b73340813f7ccf850e5
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-01-18 08:42:23 -0300

    método slice

---
+++ 1_introducao/arrays/slice.js
@@ -0,0 +1,14 @@
+let numeros = [0,1,2,3,4,5,6]
+
+console.log(numeros.slice(4));
+/* Pega do indice indicado até o final do array */
+
+console.log(numeros.slice(0,4));
+/*pega do indice do primeiro parâmetro até o indice anterior ao do último parâmetro. */
+
+console.log(numeros.slice(0,1));
+/* pega o elemento do índice 0 */
+
+console.log(numeros.slice(1,2));
+/* pega o elemento do indice 1 */
+
