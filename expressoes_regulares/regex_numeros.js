7bd7c4a8c67558f8386915b2f6709f99c3cdd428
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-02-07 09:32:35 -0300

    conjuntos

---
+++ expressoes_regulares/regex_numeros.js
@@ -0,0 +1,5 @@
+const reg1 = (/[123456]/);
+console.log(reg1.test('tem 7?'))
+
+const reg2 = /[0-9]/ ;  // maneira para descobrir se em uma frase existe números. 
+console.log(reg2.test('tem números?'));
