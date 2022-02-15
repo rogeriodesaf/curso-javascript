30cb6bf3a4bf54dc2b4e59f4ec36ea29d73271a4
Your Namerogeriodesaf <rogeriodesaf@gmail.com>
2022-02-15 10:59:30 -0300

    incluindo reject

--- programacao_assincrona/mais_Promises.js
+++ programacao_assincrona/mais_Promises.js
@@ -1,5 +1,6 @@
 function esperaAi(msg, tempo) {
     return new Promise((resolve, reject) => {
+        if(typeof msg != 'string') reject('BAD VALUE')
         setTimeout(function () {
             resolve(msg)
         }, tempo)
@@ -13,9 +14,10 @@ esperaAi('Frase 1', 1000)
     })
     .then((value) => {
         console.log(value)
-        return esperaAi('Frase 3', 2000)
+        return esperaAi(1, 2000)
     })
 .then((value)=>{console.log(value)
     return esperaAi('Frase 4', 1000)
 })
 .then((value)=>{console.log(value)})
+.catch(e=>{console.log(e)})
